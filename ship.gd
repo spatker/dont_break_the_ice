bc376ad438b21eaf57d84bf649c6128e786c98fa
Peter Suti <spatker@gmail.com>
2022-11-19 16:52:28 +0100

    Initial commit
    Signed-off-by: Peter Suti <spatker@gmail.com>

---
+++ ship.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ACCELERATE_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+	# Handle accelerate.
+	if Input.is_action_just_pressed("accelerate"):
+		velocity.y = ACCELERATE_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("steer_left", "steer_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
